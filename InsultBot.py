494c8dd299cb06bd1fd5545f187dbc2885d48e11
InterstellarTaurus <nicholas.frillman@gmail.com>
2022-08-13 08:39:34 -0400

    added comments to random_instul_reader script and function to InsultBot.py

--- InsultBot.py
+++ InsultBot.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from random_insult_reader import biggie_string
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,5 +12,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Hello'):
+        await message.channel.send(biggie_string)
 
 client.run(TOKEN)
--- random_insult_reader.py
+++ random_insult_reader.py
@@ -1,12 +1,14 @@
-ls /etc/from random import choice
+from random import choice
 
-
+# Open and read from file
 insults = open("insults.txt", "r")
 
-
+# create variable and randomly pick a string from insults.text
 line = choice(insults.readlines())
-mozza_string = (f"Yousa know what?! {line}")
-print(mozza_string)
+
+# print the string
+biggie_string = (f"Yousa know what?! {line}")
+print(biggie_string)
     
 insults.close()
 
